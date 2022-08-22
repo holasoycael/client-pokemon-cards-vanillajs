f0b79aa0f48ff8bf453c76ae652ca90eb5f23443
André Araujo <andretbt97@gmail.com>
2022-08-22 20:44:28 -0300

    feat: add event back and imagem pokemon

--- public/main.js
+++ public/main.js
@@ -10,6 +10,8 @@ window.onload = async () => {
 
   const responsePokemons = await Promise.all(Array.from(filterPokemons, async(data) => {
     const pokemon = await P.getPokemonByName(data.name)
+    console.log (pokemon)
+     
     return {
       id: pokemon.id,
       name: pokemon.name,
@@ -41,11 +43,34 @@ window.onload = async () => {
       console.log(pokemon)
 
       modal__content.innerHTML = `
-        <div>
-          <img src="${pokemon.image}" />
-        </div>
-      `
+        <div class"pokemon">
+          <div class="pokemon__head">
+            <button class="pokemon__back" type="button">
+              <ion-icon name="chevron-back-outline"></ion-icon>
+            </button>
+            <span>#${pokemon.id}</span>
+          </div>
 
+          <div class="pokemon__main">
+            <div class="pokemon__imagem">
+              <img class="pokemon__imagem" src="${pokemon.image}" height="300" />
+            </div>
+            <div class=""pokemon__info>
+              <span>${pokemon.name}</span>
+            </div>
+
+          </div>
+        </div>
+        
+      `
+      // eventos
+      const pokemon__back = document.querySelector(".pokemon__back")
+    pokemon__back.addEventListener('click', function() {
+      modal.classList.remove('modal--open')
+      modal.classList.add('modal--close')
+    })
+
+      
     })
   })
 
