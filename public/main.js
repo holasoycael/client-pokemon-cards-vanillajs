5e7962af5bbeee9807e7342a121d8cbd6dff5df1
Isaac Santana da Silva <zacksantana10@gmail.com>
2022-08-29 20:12:41 -0700

    chore: add eslint and prettier

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 const P = new Pokedex.Pokedex()
 
 function capitalizeFirstLetter(value) {
-  return value.charAt(0).toUpperCase() + value.slice(1);
+  return value.charAt(0).toUpperCase() + value.slice(1)
 }
 
 window.onload = async () => {
@@ -11,32 +11,36 @@ window.onload = async () => {
   const pokemons = getPokemons.results
 
   const filterPokemons = pokemons.filter((_, index) => index < 50)
-  
-  const responsePokemons = await Promise.all(filterPokemons.map(async (abobrinha) => {
-    const pokemon = await P.getPokemonByName(abobrinha.name)
 
-    return {
-      id: pokemon.id,
-      name: capitalizeFirstLetter(pokemon.name),
-      image: pokemon.sprites.other.home.front_default,
-      type: pokemon.types.map(({ type }) => type.name)
-    }
-  }))
+  const responsePokemons = await Promise.all(
+    filterPokemons.map(async (abobrinha) => {
+      const pokemon = await P.getPokemonByName(abobrinha.name)
 
-  const cardsHtml = responsePokemons.map((pokemon) => `
+      return {
+        id: pokemon.id,
+        name: capitalizeFirstLetter(pokemon.name),
+        image: pokemon.sprites.other.home.front_default,
+        type: pokemon.types.map(({ type }) => type.name)
+      }
+    })
+  )
+
+  const cardsHtml = responsePokemons.map(
+    (pokemon) => `
     <div class="card" data-id="${pokemon.id}">
       <img src="${pokemon.image}" width="50" height="50" />
       <span>${pokemon.name}</span>
     </div>
-  `)
+  `
+  )
 
   html.innerHTML = cardsHtml.join('\n')
 
   // NOTE: Evento para abrir o modal ao clicar no card
-  const cardAll = document.querySelectorAll(".card")
-  cardAll.forEach(function(abobrinha) {
-    abobrinha.addEventListener('click', function() {
-      const modal = document.querySelector(".modal")
+  const cardAll = document.querySelectorAll('.card')
+  cardAll.forEach(function (abobrinha) {
+    abobrinha.addEventListener('click', function () {
+      const modal = document.querySelector('.modal')
       modal.classList.remove('modal--close')
       modal.classList.add('modal--open')
 
@@ -55,17 +59,25 @@ window.onload = async () => {
 
           <div class="pokemon__main">
             <div class="pokemon__imagem">
-              <div class="pokemon__blur" style="background-image: url(${pokemon.image});"></div>
-              <img class="pokemon__imagem" src="${pokemon.image}" height="300" />
+              <div class="pokemon__blur" style="background-image: url(${
+                pokemon.image
+              });"></div>
+              <img class="pokemon__imagem" src="${
+                pokemon.image
+              }" height="300" />
             </div>
             <div class="pokemon__info">
               <span class="pokemon__name">${pokemon.name}</span>
               <div class="pokemon__types">
-                ${pokemon.type.map ((type) => `
+                ${pokemon.type
+                  .map(
+                    (type) => `
                   <p class="pokemon__typeName pokemon__typeName--${type}">
                     ${capitalizeFirstLetter(type)} 
                   </p>
-                `).join('\n')}
+                `
+                  )
+                  .join('\n')}
               </div>
             </div>
 
@@ -73,13 +85,11 @@ window.onload = async () => {
         </div>
       `
       // eventos
-      const pokemon__back = document.querySelector(".pokemon__back")
-    pokemon__back.addEventListener('click', function() {
-      modal.classList.remove('modal--open')
-      modal.classList.add('modal--close')
-    })
-
-      
+      const pokemon__back = document.querySelector('.pokemon__back')
+      pokemon__back.addEventListener('click', function () {
+        modal.classList.remove('modal--open')
+        modal.classList.add('modal--close')
+      })
     })
   })
 
@@ -90,8 +100,8 @@ window.onload = async () => {
 
   // NOTE: Fecha todos os modal
   const modal__close = document.querySelector('.modal__close')
-  modal__close.addEventListener('click', function() {
-    const modal = document.querySelector(".modal")
+  modal__close.addEventListener('click', function () {
+    const modal = document.querySelector('.modal')
     modal.classList.remove('modal--open')
     modal.classList.add('modal--close')
   })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,110 @@
-console.log('NOVO PROJETO!'
+const P = new Pokedex.Pokedex()
 
-)
+function capitalizeFirstLetter(value: string) {
+  return value.charAt(0).toUpperCase() + value.slice(1)
+}
 
-const data = 'ola';
+window.onload = async () => {
+  const html = document.querySelector('.wrapper')
 
+  const getPokemons = await P.getPokemons()
+  const pokemons = getPokemons.results
 
+  const filterPokemons = pokemons.filter((_, index) => index < 50)
+
+  const responsePokemons = await Promise.all(
+    filterPokemons.map(async (abobrinha) => {
+      const pokemon = await P.getPokemonByName(abobrinha.name)
+
+      return {
+        id: pokemon.id,
+        name: capitalizeFirstLetter(pokemon.name),
+        image: pokemon.sprites.other.home.front_default,
+        type: pokemon.types.map(({ type }) => type.name)
+      }
+    })
+  )
+
+  const cardsHtml = responsePokemons.map(
+    (pokemon) => `
+    <div class="card" data-id="${pokemon.id}">
+      <img src="${pokemon.image}" width="50" height="50" />
+      <span>${pokemon.name}</span>
+    </div>
+  `
+  )
+
+  if (html) html.innerHTML = cardsHtml.join('\n')
+
+  // NOTE: Evento para abrir o modal ao clicar no card
+  const cardAll = document.querySelectorAll('.card')
+  cardAll.forEach(function (abobrinha) {
+    abobrinha.addEventListener('click', function () {
+      const modal = document.querySelector('.modal')
+      modal!.classList.remove('modal--close')
+      modal?.classList.add('modal--open')
+
+      const useId = parseFloat(this.dataset.id)
+      const pokemon = responsePokemons.find((data) => data.id === useId)
+      const modal__content = document.querySelector('.modal__content')
+
+      if (modal__content) {
+        modal__content.innerHTML = `
+        <div class="pokemon">
+          <div class="pokemon__head">
+            <button class="pokemon__back" type="button">
+              <ion-icon name="chevron-back-outline"></ion-icon>
+            </button>
+            <span class="pokemon__id">#${pokemon.id}</span>
+          </div>
+
+          <div class="pokemon__main">
+            <div class="pokemon__imagem">
+              <div class="pokemon__blur" style="background-image: url(${
+                pokemon.image
+              });"></div>
+              <img class="pokemon__imagem" src="${
+                pokemon.image
+              }" height="300" />
+            </div>
+            <div class="pokemon__info">
+              <span class="pokemon__name">${pokemon.name}</span>
+              <div class="pokemon__types">
+                ${pokemon.type
+                  .map(
+                    (type) => `
+                  <p class="pokemon__typeName pokemon__typeName--${type}">
+                    ${capitalizeFirstLetter(type)} 
+                  </p>
+                `
+                  )
+                  .join('\n')}
+              </div>
+            </div>
+
+          </div>
+        </div>
+      `
+      }
+      // eventos
+      const pokemon__back = document.querySelector('.pokemon__back')
+      pokemon__back.addEventListener('click', function () {
+        modal.classList.remove('modal--open')
+        modal.classList.add('modal--close')
+      })
+    })
+  })
+
+  // ELEMENTO .ADD_EVENTO('EVENTO DE CLICK', () => {
+
+  //   // meu codigo
+  // })
+
+  // NOTE: Fecha todos os modal
+  const modal__close = document.querySelector('.modal__close')
+  modal__close.addEventListener('click', function () {
+    const modal = document.querySelector('.modal')
+    modal.classList.remove('modal--open')
+    modal.classList.add('modal--close')
+  })
+}
